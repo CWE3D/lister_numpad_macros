71a60b15e366f86379a3d6e26cc6a0df186005a1
Jason Schoeman <jason@schoeman.me>
2024-11-04 10:40:47 +0200

    Modified command mapping to better pick up events.

--- extras/numpad_macros.py
+++ extras/numpad_macros.py
@@ -176,39 +176,22 @@ class NumpadMacros:
         return mapping
 
     def _initialize_command_mapping(self) -> Dict[str, str]:
-        """Initialize the key to command mapping with default RESPOND messages for unassigned keys"""
-        mapping = {
-            # Set all keys to default "not assigned" messages first
-            "1": "RESPOND MSG=\"Key 1 not assigned yet\"",
-            "2": "RESPOND MSG=\"Key 2 not assigned yet\"",
-            "3": "RESPOND MSG=\"Key 3 not assigned yet\"",
-            "4": "RESPOND MSG=\"Key 4 not assigned yet\"",
-            "5": "RESPOND MSG=\"Key 5 not assigned yet\"",
-            "6": "RESPOND MSG=\"Key 6 not assigned yet\"",
-            "7": "RESPOND MSG=\"Key 7 not assigned yet\"",
-            "8": "RESPOND MSG=\"Key 8 not assigned yet\"",
-            "9": "RESPOND MSG=\"Key 9 not assigned yet\"",
-            "0": "RESPOND MSG=\"Key 0 not assigned yet\"",
-            "dot": "RESPOND MSG=\"Key dot not assigned yet\"",
-            "enter": "RESPOND MSG=\"Key enter not assigned yet\"",
-            "grave": "RESPOND MSG=\"Key grave not assigned yet\"",
-            "up": "RESPOND MSG=\"Key up not assigned yet\"",
-            "down": "RESPOND MSG=\"Key down not assigned yet\"",
+        """Initialize the key to command mapping"""
+        mapping = {}
 
-            # Alternative mode keys (volume button press)
-            "1_alt": "RESPOND MSG=\"Key 1 (alt mode) not assigned yet\"",
-            "2_alt": "RESPOND MSG=\"Key 2 (alt mode) not assigned yet\"",
-            "3_alt": "RESPOND MSG=\"Key 3 (alt mode) not assigned yet\"",
-            "4_alt": "RESPOND MSG=\"Key 4 (alt mode) not assigned yet\"",
-            "5_alt": "RESPOND MSG=\"Key 5 (alt mode) not assigned yet\"",
-            "6_alt": "RESPOND MSG=\"Key 6 (alt mode) not assigned yet\"",
-            "7_alt": "RESPOND MSG=\"Key 7 (alt mode) not assigned yet\"",
-            "8_alt": "RESPOND MSG=\"Key 8 (alt mode) not assigned yet\"",
-            "9_alt": "RESPOND MSG=\"Key 9 (alt mode) not assigned yet\"",
-            "0_alt": "RESPOND MSG=\"Key 0 (alt mode) not assigned yet\"",
-            "dot_alt": "RESPOND MSG=\"Key dot (alt) not assigned yet\"",
-            "enter_alt": "RESPOND MSG=\"Key enter (alt) not assigned yet\"",
-        }
+        # Get all configured keys
+        for key in [
+            'key_1', 'key_2', 'key_3', 'key_4', 'key_5',
+            'key_6', 'key_7', 'key_8', 'key_9', 'key_0',
+            'key_dot', 'key_enter', 'key_grave',
+            'key_1_alt', 'key_2_alt', 'key_3_alt', 'key_4_alt', 'key_5_alt',
+            'key_6_alt', 'key_7_alt', 'key_8_alt', 'key_9_alt', 'key_0_alt',
+            'key_dot_alt', 'key_enter_alt'
+        ]:
+            # Get the command from config, with default "not assigned" message
+            command = self.printer.lookup_object('configfile').get('numpad_macros', key,
+                                                                   f"RESPOND MSG=\"{key} not assigned yet\"")
+            mapping[key] = command
 
         if self.debug_log:
             self._debug_log(f"Initialized command mapping: {mapping}")
