bbe39b0f316fa75498b2b85a16d61539575ea0f5
Jason Schoeman <jason@schoeman.me>
2024-11-07 11:38:50 +0200

    Needed underscore

--- components/numpad_macros.py
+++ components/numpad_macros.py
@@ -91,7 +91,7 @@ class NumpadMacros:
             # Store the command exactly as configured
             self.command_mapping[key] = config.get(key, f"RESPOND MSG=\"{key} not assigned\"")
             # Create QUERY version by adding prefix
-            self.query_mapping[key] = f"QUERY_{self.command_mapping[key]}"
+            self.query_mapping[key] = f"_QUERY{self.command_mapping[key]}"
 
             if self.debug_log:
                 await self._execute_gcode(
